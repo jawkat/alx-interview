704fdf6c7b3f97c2a946ef08db34b7e169e1ecd3
jawkat <kad.joliano@gmail.com>
2024-08-02 01:18:30 +0100

    task 0

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,24 @@
+def canUnlockAll(boxes):
+    """
+    Détermine si toutes les boîtes peuvent être ouvertes.
+
+    :param boxes: Liste de listes, où chaque sous-liste contient des clés.
+    :return: True si toutes les boîtes peuvent être ouvertes, sinon False.
+    """
+    n = len(boxes)  # Nombre total de boîtes
+    opened = [False] * n  # Liste pour suivre les boîtes ouvertes
+    opened[0] = True  # La boîte 0 est ouverte
+
+    to_explore = [0]  # Commencer avec la boîte 0
+
+    while to_explore:
+        current_box = to_explore.pop()  # Prendre une boîte de la pile
+        for key in boxes[current_box]:
+            if key < n and not opened[key]:
+                opened[key] = True  # Marquer la boîte comme ouverte
+                to_explore.append(key)  # Ajouter cette boîte à la pile
+
+    for status in opened:
+        if not status:
+            return False  # Si une boîte n'est pas ouverte, retourner False
+    return True  # Si toutes les boîtes sont ouvertes, retourner True
